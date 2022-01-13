b817ed4ad7bf659dfe47ba8fe52264f5414b47ad
Alharb7 <76629405+hassanalharbi123@users.noreply.github.com>
2022-01-13 23:25:50 +0300

    Update start.py

--- start.py
+++ start.py
@@ -1,23 +1,26 @@
-import requests
-import pyfiglet
-import termcolor
-
+import requests,pyfiglet,termcolor
+from optparse import OptionParser
+#___________________________ OPT ___________________________
+use=OptionParser('\033[01;31mCommand\033[00;37m')
+use.add_option("-t","--target",dest="target",help="write the Target like this : \033[01;31m https://example.com/index.php?id=")
+(options,args) = use.parse_args()
+#___________________________ OPT ___________________________
 TL_NAME_text = pyfiglet.figlet_format("SQLI CHECK")
 TL_NAME = termcolor.colored(TL_NAME_text, 'red')
 
 print(TL_NAME) 
 print(termcolor.colored('Created by izox\ntwitter : @izox99\n', 'blue'))
-print(termcolor.colored('write the link like this : https://example.com/index.php?id=1\n', 'red'))
 
 dictionary = open('dict.txt', 'r')
-url = input("please enter a URL : ")
+url = options.target
 try:
     for x in dictionary:
-        r = requests.get(url + x.rstrip())
-        text = r.text
-        result = "<b>Warning</b>:" in text
+        r = requests.get(url + x.rstrip()).text
+        result = "mysql" in r
         if result == True:
-            print(termcolor.colored("[+] This page " + url + " is infected", 'green'))
+            print(termcolor.colored("[+] The Payload is: " + x, 'green'))
             break
+        else:
+            print(termcolor.colored("[+] Payload is not Found" + x, 'red'))
 except Exception:
     print(termcolor.colored("", 'green'))
