bf8ac4722fe825696908d9e5384b6043c1b7223e
khaled <80907063+ik9@users.noreply.github.com>
2022-01-13 14:47:12 +0300

    files
    files

---
+++ start.py
@@ -0,0 +1,26 @@
+import requests
+import pyfiglet
+import termcolor
+
+TL_NAME_text = pyfiglet.figlet_format("SQLI CHECK")
+TL_NAME = termcolor.colored(TL_NAME_text, 'red')
+
+print(TL_NAME) 
+print(termcolor.colored('Created by izox\ntwitter : @izox99\n', 'blue'))
+print(termcolor.colored('write the link like this : https://example.com/index.php?id=1\n', 'red'))
+
+dictionary = open('dict.txt', 'r')
+url = input("please enter a URL : ")
+try:
+    for x in dictionary:
+        r = requests.get(url + x.rstrip())
+        text = r.text
+        result = "<b>Warning</b>:" in text
+        if result == True:
+            print(termcolor.colored("[+] This page " + url + " is infected", 'green'))
+            break
+        # else:
+        #     print("SC : ",r.status_code)
+        #     print("Sorry, " + url + "/" + x)
+except Exception:
+    print(termcolor.colored("", 'green'))
