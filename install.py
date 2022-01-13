3000d19d5bb3a30e037d17f66ae638de125b1649
Alharb7 <76629405+hassanalharbi123@users.noreply.github.com>
2022-01-13 23:26:39 +0300

    Update install.py

--- install.py
+++ install.py
@@ -1,27 +1,8 @@
-#Programming By GitHub:xF14x
-
 from os import system
-
-print(""" Welcome To \"install.py\" File
-
-	The File Will Install for you:
-	1- requests
-	2- pyfiglet
-	3- termcolor
-
-	Windows (1)
-	Linux (2)
-""")
-
-install = input("==>")
-
-if install == "1":
-	system("pip install requests")
-	system("pip install pyfiglet")
-	system("pip install termcolor")
-elif install == "2":
-	system("pip3 install requests")
-	system("pip3 install pyfiglet")
-	system("pip3 install termcolor")
-else:
-	exit("Error, Please ReOpen The install.py File And Choose Your OS")
+from sys import platform
+list=['requests','pyfiglet','termcolor']
+for pip in list:
+        if "linux" in platform:
+                system(f'pip3 install {pip}')
+        else:
+                system(f'pip install {pip}')
